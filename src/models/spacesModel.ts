ae76a6a9e00fbc440e38c804e68a5ac2fa64eafe
HayHay <dev@hayhay.dev>
2024-12-22 02:27:53 -0500

    Add notification time to spaces model

--- src/models/spacesModel.ts
+++ src/models/spacesModel.ts
@@ -1,5 +1,12 @@
 import { z } from "@hono/zod-openapi";
-import { pgTable, serial, text, timestamp, uuid } from "drizzle-orm/pg-core";
+import {
+  jsonb,
+  pgTable,
+  serial,
+  text,
+  timestamp,
+  uuid,
+} from "drizzle-orm/pg-core";
 import { createSelectSchema } from "drizzle-zod";
 
 export enum SpaceType {
@@ -16,10 +23,17 @@ export const spacesTable = pgTable("spaces", {
   title: text("title").notNull(),
   spaceType: text("spaceType").notNull().default(SpaceType.LIVING_ROOM),
   userId: uuid("user_id").notNull(),
+  notificationTimes: jsonb("notificationTimes")
+    .notNull()
+    .default({ standard: [12, 24], prioritized: [48] }),
 });
 
 export const selectSpaceSchema = createSelectSchema(spacesTable, {
   id: z.string().transform((v) => parseInt(v, 10)),
+  notificationTimes: z.object({
+    standard: z.array(z.number()),
+    prioritized: z.array(z.number()),
+  }),
 });
 
 export const selectSpacesArraySchema = z.array(selectSpaceSchema);
